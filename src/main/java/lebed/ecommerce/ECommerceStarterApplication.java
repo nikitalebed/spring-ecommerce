613c1d4f25d4064cc3a6cd8e452d343e5470a9b9
Mykyta Lebid <Mykyta_Lebid@epam.com>
2017-11-06 13:28:41 +0200

    create empty spring boot web app

---
+++ src/main/java/lebed/ecommerce/ECommerceStarterApplication.java
@@ -0,0 +1,13 @@
+package lebed.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceStarterApplication.class, args);
+    }
+
+}
