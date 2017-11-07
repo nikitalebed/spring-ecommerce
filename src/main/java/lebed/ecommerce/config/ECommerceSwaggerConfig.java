5671424fe319de0fef38d99a0bb5175aa20951f1
Mykyta Lebid <Mykyta_Lebid@epam.com>
2017-11-07 20:23:23 +0200

    add spring swagger

---
+++ src/main/java/lebed/ecommerce/config/ECommerceSwaggerConfig.java
@@ -0,0 +1,28 @@
+package lebed.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class ECommerceSwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(or(regex("/product.*"),
+                        regex("/group.*"),
+                        regex("/order.*"),
+                        regex("/cart.*")))
+                .build();
+    }
+}
