3959ebd990cf6d81b3f57ee8a52712cfcaaf3241
Mykyta Lebid <Mykyta_Lebid@epam.com>
2017-11-09 12:54:36 +0200

    add from-end react Hello World

---
+++ src/main/java/lebed/ecommerce/controller/HomeController.java
@@ -0,0 +1,14 @@
+package lebed.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    String home() {
+        return "index";
+    }
+
+}
