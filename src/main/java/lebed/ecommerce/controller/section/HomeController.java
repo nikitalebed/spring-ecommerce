c96b3b2f2567946543277a936456fb2407ae2015
nickitalebed1 <simple199224@gmail.com>
2018-02-08 15:22:50 +0200

    add latest news to the home page

--- src/main/java/lebed/ecommerce/controller/section/HomeController.java
+++ src/main/java/lebed/ecommerce/controller/section/HomeController.java
@@ -2,15 +2,33 @@ package lebed.ecommerce.controller.section;
 
 import lebed.ecommerce.config.web.Navigation;
 import lebed.ecommerce.config.web.Section;
+import lebed.ecommerce.model.News;
+import lebed.ecommerce.repository.NewsRepository;
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Navigation(Section.HOME)
 public class HomeController {
 
+    @Autowired
+    private NewsRepository newsRepository;
+
     @RequestMapping("/")
-    String home() {
+    String home(Model model) {
+        List<News> latestNews =
+                ((List<News>) newsRepository.findAll())
+                        .stream()
+                        .filter(n -> DateUtils.isSameDay(n.getCreateDate(), new Date()))
+                        .collect(Collectors.toList());
+        model.addAttribute("latestNews", latestNews);
         return "index";
     }
 
